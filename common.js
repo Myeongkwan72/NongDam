e9dbd4ce0b5fd0a912f7776af226d9b3428e9455
a01040579861 <a01040579861@gmail.com>
2025-04-08 14:59:56 +0900

    250408 detail commit

--- common.js
+++ common.js
@@ -1,3 +1,10 @@
+/* logo location */
+const logoBtn = document.querySelector("#header_logo");
+logoBtn.addEventListener("click", () => {
+  location.href = "../index.html";
+});
+
+/* trigger menu */
 const triBtn = document.querySelector(".trigger");
 triBtn.addEventListener("click", (e) => {
   e.currentTarget.classList.toggle("active");
